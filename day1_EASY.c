0efea08ba326e416ca2c11ba8f8a844894191b51
zain-cmd-hub <abbaskhan896241@gmail.com>
2024-09-28 11:17:28 +0530

    Add files via upload

---
+++ day1_EASY.c
@@ -0,0 +1,17 @@
+  #include <stdio.h>
+
+int main() {
+   int x;
+
+   printf("Enter the score of a TEAM :");
+   scanf("%d",&x);
+
+   if(x>=12){
+    printf("YES");
+   }
+   else{
+    printf("NO");
+   }
+    
+    return 0;
+}
