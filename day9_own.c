0ccd261f6d6e57fd52b3f0c3832ece5b6f3dc8bc
Mohammad Abbas <abbaskhan896241@gmail.com>
2024-10-06 11:22:34 +0530

    Add files via upload

---
+++ day9_own.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int a = 10;
+   float b = 69.7;
+   char c = 'A' ;
+   
+   printf("integer value : %d\n", a);
+   printf("float value : %2f\n", b);
+   printf("character value : %c\n", c);
+
+    return 0;
+}
