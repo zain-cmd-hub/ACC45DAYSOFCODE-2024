11683996a1fb1d05cf970f7aad740fbb84ad5f19
Mohammad Abbas <abbaskhan896241@gmail.com>
2024-11-02 22:38:01 +0530

    Add files via upload

---
+++ day31_easy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+        int choices = N * (N - 1);
+        printf("%d\n", choices);
+    }
+    return 0;
+}
